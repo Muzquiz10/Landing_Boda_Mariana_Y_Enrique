f3086e909919fb011911707a46d935cd4263a701
Muzquiz10 <muzquiz.10@gmail.com>
2025-12-13 18:37:03 +0100

    prueba padrinos

--- js/main.js
+++ js/main.js
@@ -21,8 +21,8 @@ if (type === "amigos") {
 }
 
 if (type === "padrinos"){
-  message = "Querido padrino y acompañate/s\n"
-             "Prueba"
+  message = "Querido padrino y acompañate/s\n
+             Prueba"
 }
 
 messageEl.innerText = message;
