5946bd75f31fa5fe477e5982b529e68ac7e17bf1
Muzquiz10 <muzquiz.10@gmail.com>
2025-12-14 10:10:33 +0100

    efecto añadido

--- js/main.js
+++ js/main.js
@@ -1,98 +1,116 @@
-const params = new URLSearchParams(window.location.search);
+// const params = new URLSearchParams(window.location.search);
 
-let name = params.get("name");
-const type = params.get("type");
+// let name = params.get("name");
+// const type = params.get("type");
 
-// Reemplazar los guiones bajos por espacios
-if (name) {
-  name = name.replace(/_/g, " "); 
-}
+// // Reemplazar los guiones bajos por espacios
+// if (name) {
+//   name = name.replace(/_/g, " "); 
+// }
 
-const title = document.getElementById("title");
-const messageEl = document.getElementById("message");
+// const title = document.getElementById("title");
+// const messageEl = document.getElementById("message");
 
-if (name) {
+// if (name) {
   
-  title.innerText = `${name}`;
-  // title.innerText = `Invitación de boda de Mariana y Enrique para ${name} 💍`;
+//   title.innerText = `${name}`;
+//   // title.innerText = `Invitación de boda de Mariana y Enrique para ${name} 💍`;
   
-}
+// }
 
-let message = "Queremos compartir contigo un día muy especial para nosotros.";
+// let message = "Queremos compartir contigo un día muy especial para nosotros.";
 
-if (type === "familia") {
-  message = "Querido miembro de la familia,\n\nNos llena de felicidad invitarte a nuestra boda y ser parte de este momento tan significativo en nuestras vidas.\nA continuación te dejamos los detalles.";
-}
+// if (type === "familia") {
+//   message = "Querido miembro de la familia,\n\nNos llena de felicidad invitarte a nuestra boda y ser parte de este momento tan significativo en nuestras vidas.\nA continuación te dejamos los detalles.";
+// }
 
-if (type === "amigos") {
-  message = "Querido amigo/a,\n\nMuchas gracias por todas las vivencias que hemos compartido juntos y esperamos poder contar con tu presencia en el día de nuestra boda.\nA continuación te dejamos los detalles.";
-}
+// if (type === "amigos") {
+//   message = "Querido amigo/a,\n\nMuchas gracias por todas las vivencias que hemos compartido juntos y esperamos poder contar con tu presencia en el día de nuestra boda.\nA continuación te dejamos los detalles.";
+// }
 
-if (type === "padrino_Jhan"){
-  message = "Querido padrino y acompañantes,\n\nComo persona especial, nos complace informarte a ti a Cinthia, Arely y Anita de los detalles de nuestra boda.";
-}
+// if (type === "padrino_Jhan"){
+//   message = "Querido padrino y acompañantes,\n\nComo persona especial, nos complace informarte a ti a Cinthia, Arely y Anita de los detalles de nuestra boda.";
+// }
 
-if (type === "padrino_Luis"){
-  message = "Querido padrino y acompañate,\n\nComo persona especial, nos complace informarte a ti y a Maricarmen de los detalles de nuestra boda.";
-}
+// if (type === "padrino_Luis"){
+//   message = "Querido padrino y acompañate,\n\nComo persona especial, nos complace informarte a ti y a Maricarmen de los detalles de nuestra boda.";
+// }
 
-if (type === "mama"){
-  message = "Querida mamá,\n\nComo persona especial, nos complace informarte los detalles de nuestra boda.";
-}
+// if (type === "mama"){
+//   message = "Querida mamá,\n\nComo persona especial, nos complace informarte los detalles de nuestra boda.";
+// }
 
-if (type === "hermano"){
-  message = "Querido hermano y Esme,\n\nComo persona especial, nos complace informarte de los detalles de nuestra boda.";
-}
+// if (type === "hermano"){
+//   message = "Querido hermano y Esme,\n\nComo persona especial, nos complace informarte de los detalles de nuestra boda.";
+// }
 
-messageEl.innerText = message;
+// messageEl.innerText = message;
 
 // NEW CODE
 
-// document.addEventListener("DOMContentLoaded", () => {
-//   const params = new URLSearchParams(window.location.search);
-//   let name = params.get("name");
-//   const type = params.get("type");
+document.addEventListener("DOMContentLoaded", () => {
 
-//   // Reemplazar guiones bajos por espacios
-//   if (name) {
-//     name = name.replace(/_/g, " "); 
-//   }
+  /* ===============================
+     1. PERSONALIZACIÓN POR URL
+  =============================== */
 
-//   const title = document.getElementById("title");
-//   const messageEl = document.getElementById("message");
+  const params = new URLSearchParams(window.location.search);
 
-//   if (name) {
-//     title.innerText = `${name}`;
-//     // title.innerText = `Invitación de boda de Mariana y Enrique para ${name} 💍`;
-//   }
+  let name = params.get("name");
+  const type = params.get("type");
 
-//   let message = "Queremos compartir contigo un día muy especial para nosotros.";
+  // Reemplazar guiones bajos por espacios
+  if (name) {
+    name = name.replace(/_/g, " ");
+  }
 
-//   if (type === "familia") {
-//     message = "Querido miembro de la familia,\n\nNos llena de felicidad invitarte a nuestra boda y ser parte de este momento tan significativo en nuestras vidas.\nA continuación te dejamos los detalles de nuestra boda.";
-//   }
+  const title = document.getElementById("title");
+  const messageEl = document.getElementById("message");
 
-//   if (type === "amigos") {
-//     message = "Querido amigo/a,\n\nMuchas gracias por todas las vivencias que hemos compartido juntos y esperamos poder contar con tu presencia en el día de nuestra boda.\nA continuación te dejamos los detalles de nuestra boda.";
-//   }
+  if (name) {
+    title.innerText = `${name}`;
+  }
 
-//   if (type === "padrino_Jhan") {
-//     message = "Querido padrino y acompañantes,\n\nComo persona especial, nos complace informarte a ti a Cinthia, Arely y Anita de los detalles de nuestra boda.";
-//   }
+  let message = "Queremos compartir contigo un día muy especial para nosotros.";
 
-//   if (type === "padrino_Luis") {
-//     message = "Querido padrino y acompañate,\n\nComo persona especial, nos complace informarte a ti y a Maricarmen de los detalles de nuestra boda.";
-//   }
+  if (type === "familia") {
+    message = "Querido miembro de la familia,\n\nNos llena de felicidad invitarte a nuestra boda y ser parte de este momento tan significativo en nuestras vidas.\nA continuación te dejamos los detalles.";
+  }
 
-//   if (type === "mama") {
-//     message = "Querida mamá,\n\nComo persona especial, nos complace informarte los detalles de nuestra boda.";
-//   }
+  if (type === "amigos") {
+    message = "Querido amigo/a,\n\nMuchas gracias por todas las vivencias que hemos compartido juntos y esperamos poder contar con tu presencia en el día de nuestra boda.\nA continuación te dejamos los detalles.";
+  }
 
-//   if (type === "hermano") {
-//     message = "Querido hermano y Esme,\n\nComo persona especial, nos complace informarte de los detalles de nuestra boda.";
-//   }
+  if (type === "padrino_Jhan") {
+    message = "Querido padrino y acompañantes,\n\nComo persona especial, nos complace informarte a ti, a Cinthia, Arely y Anita de los detalles de nuestra boda.";
+  }
 
-//   messageEl.innerText = message;
-// });
+  if (type === "padrino_Luis") {
+    message = "Querido padrino y acompañante,\n\nComo persona especial, nos complace informarte a ti y a Maricarmen de los detalles de nuestra boda.";
+  }
 
+  if (type === "mama") {
+    message = "Querida mamá,\n\nComo persona especial, nos complace informarte los detalles de nuestra boda.";
+  }
+
+  if (type === "hermano") {
+    message = "Querido hermano y Esme,\n\nComo persona especial, nos complace informarte de los detalles de nuestra boda.";
+  }
+
+  messageEl.innerText = message;
+
+
+  /* ===============================
+     2. ANIMACIÓN GLOBAL DE ENTRADA
+  =============================== */
+
+  const reveals = document.querySelectorAll(".reveal");
+
+  reveals.forEach((el, index) => {
+    setTimeout(() => {
+      el.classList.add("active");
+    }, index * 900); // efecto en cascada
+  });
+
+});
 
